95eca279bc3b3f878c9c2becb61469e3055f4892
lalit <lalitdeore12@gmail.com>
2023-10-05 18:57:03 +0530

    nav bar updatd

--- frontend/src/components/LeftNav.jsx
+++ frontend/src/components/LeftNav.jsx
@@ -27,11 +27,7 @@ const LeftNav = () => {
   };
 
   return (
-    <div
-      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
-        mobileMenu ? "translate-x-0" : ""
-      }`}
-    >
+    <div className={`navbar-container ${mobileMenu ? "nav-container" : ""}`}>
       <div className="left-nav">
         {categories.map((item) => {
           return (
